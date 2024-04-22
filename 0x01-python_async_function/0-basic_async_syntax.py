be19d6cf959a50a62d1fff1688e82ab2259a5c26
Abdullah <abdallahmosbah25@gmail.com>
2024-04-22 16:01:55 +0200

    i don't know

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python3
-""" async function Based """
-
-import asyncio
+""" Async function example using asyncio. """
 import random
+import asyncio
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """ waiting for random amount of time """
-
+    """ Wait for a random amount of time between 0 and max_delay seconds. """
     random_number = random.uniform(0, max_delay)
     await asyncio.sleep(random_number)
-
     return random_number
