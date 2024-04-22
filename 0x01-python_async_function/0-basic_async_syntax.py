114334292fbe268fa2a3db6b20b8ecb0d1c45834
Abdullah <abdallahmosbah25@gmail.com>
2024-04-22 16:03:13 +0200

    remove types

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 import asyncio
 
 
-async def wait_random(max_delay: int = 10) -> float:
+async def wait_random(max_delay=10):
     """ Wait for a random amount of time between 0 and max_delay seconds. """
     random_number = random.uniform(0, max_delay)
     await asyncio.sleep(random_number)
