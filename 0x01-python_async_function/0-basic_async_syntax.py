4f6e07062c5ae699f0d35069fd3e733c1e5d9b21
Abdullah <abdallahmosbah25@gmail.com>
2024-04-22 15:55:20 +0200

    fix by adding types

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -5,7 +5,7 @@ import asyncio
 import random
 
 
-async def wait_random(max_delay=10):
+async def wait_random(max_delay: int =10) -> float:
     """ waiting for random amount of time """
 
     random_number = random.uniform(0, max_delay)
