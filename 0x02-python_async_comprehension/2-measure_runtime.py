1c9ea347da25142230a4de52a8b866b4ba7273c3
Abdullah <abdallahmosbah25@gmail.com>
2024-04-23 23:28:37 +0200

    final task deleting white spaces

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -8,10 +8,10 @@ async_comprehension = __import__('1-async_comprehension').async_comprehension
 async def measure_runtime() -> float:
     """ Run time for four parallel comprehensions"""
     start = time.time()
-    
+
     await asyncio.gather(async_comprehension(), async_comprehension(),
                          async_comprehension(), async_comprehension())
 
     end = time.time()
-    
+
     return end - start
